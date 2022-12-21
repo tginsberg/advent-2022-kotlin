625eaca16f7c4bea13c6c2a66485901ab581ebaf
Todd Ginsberg <todd@ginsberg.com>
2022-12-20 21:03:32 -0500

    Day 20: Grove Positioning System

--- src/test/kotlin/com/ginsberg/advent2022/Day20Test.kt
+++ src/test/kotlin/com/ginsberg/advent2022/Day20Test.kt
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
-@DisplayName("Day 19")
+@DisplayName("Day 20")
 class Day20Test {
 
     // Arrange
